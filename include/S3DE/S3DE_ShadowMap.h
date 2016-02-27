e2d88f3ae170d1d96959177be2e91628b6c5fdf1
Monwarez <monwarez@gmail.com>
2016-02-27 16:02:14 +0100

    Begin the shadow stage

---
+++ include/S3DE/S3DE_ShadowMap.h
@@ -0,0 +1,78 @@
+/*
+Copyright (c) 2016, Payet Thibault
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the Monwarez Inc nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL PAYET THIBAULT BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#ifndef S3DE_SHADOW_MAP_INCLUED
+#define	S3DE_SHADOW_MAP_INCLUED
+
+#include <GL/glew.h>
+#include <S3DE_Shader.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+namespace S3DE
+{
+	class	ShadowMapFBO
+	{
+		public:
+			ShadowMapFBO();
+			~ShadowMapFBO();
+			/** \brief initialize a shadow map with the given size
+			*	\param width	width of the FBO
+			*	\param height	height of the FBO
+			*/
+			void	Init(unsigned int width, unsigned int height); // can launch an exception
+			/** \brief bind the Shadow map FBO to the current state in writing mode*/
+			void	BindForWriting();
+			/** \brief bind the shadow map for reading 
+			*	\param	TexrureUnit	the type of the texture to be attached
+			*/
+			void	BindForReading(GLenum	TextureUnit);
+		private:
+			GLuint		m_fbo;	//< identifier for the fbo
+			GLuint		m_shadowMap; //< identifier for the shadow map
+
+	};
+	class	ShadowMapShader : public Shader
+	{
+		public:
+			ShadowMapShader();
+			/** \brief	Init the shader for uniform location*/
+			void	Init();
+			/** \brief Set the world view projection matrix to use
+			* \param wvp the world view projection matrix
+			*/
+			void	SetWVP(glm::mat4 const & wvp);
+			/** \brief	Set the texture unit to use
+			*	\param	TextureUnit the texture unit to use
+			*/
+			void	SetTextureUnit(unsigned int TextureUnit);
+		private:
+			GLuint	m_wvpLocation;
+			GLuint	m_textureLocation;
+	};
+
+}
+#endif
---
+++ src/engine/ShadowMap.cpp
@@ -0,0 +1,112 @@
+/*
+Copyright (c) 2016, Payet Thibault
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the Monwarez Inc nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL PAYET THIBAULT BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#include <S3DE_ShadowMap.h>
+
+// for exception just launch a string
+#include <string>
+
+using namespace S3DE;
+ShadowMapFBO::ShadowMapFBO():m_fbo(0),m_shadowMap(0)
+{
+}
+ShadowMapFBO::~ShadowMapFBO()
+{
+	if (m_fbo	!= 0)
+	{
+		glDeleteFramebuffers(1, &m_fbo);
+	}
+	if (m_shadowMap != 0)
+	{
+		glDeleteTextures(1, &m_shadowMap);
+	}
+}
+void	ShadowMapFBO::Init(unsigned int width, unsigned int height)
+{
+	// Generate the framebuffer
+	glGenFramebuffers(1,&m_fbo);
+
+	// Create the depth buffer
+	glGenTextures(1,&m_shadowMap);
+	glBindTexture(GL_TEXTURE_2D, m_shadowMap);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, width, height, 0, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+	glBindFramebuffer(GL_FRAMEBUFFER, m_fbo);
+		glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, m_shadowMap, 0);
+		
+		glDrawBuffer(GL_NONE);
+		glReadBuffer(GL_NONE);
+
+		auto	status	=	glCheckFramebufferStatus(GL_FRAMEBUFFER);
+	if (status != GL_FRAMEBUFFER_COMPLETE)
+	{
+		switch(status)
+		{
+			case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
+				throw std::string("GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT: Not all framebuffer attachment points \
+									 are framebuffer attachment complete");
+				break;
+			case GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS_EXT:
+				throw std::string("GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS: Not all attached images have the same width and height");
+				break;
+			case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
+				throw std::string("GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT: No images are attached to the framebuffer");
+				break;
+			case GL_FRAMEBUFFER_UNSUPPORTED:
+				throw std::string("GL_FRAMEBUFFER_UNSUPPORTED: The combinaison of internal formats of the attached images \
+									violates an implementation-dependent set of restriction");
+				break;
+			default:
+				throw std::string ("unknow framebuffer error");
+		}
+	}
+}
+void	ShadowMapFBO::BindForWriting()
+{
+	glBindFramebuffer(GL_FRAMEBUFFER, m_fbo);
+}
+void	ShadowMapFBO::BindForReading(GLenum TextureUnit)
+{
+	glActiveTexture(TextureUnit);
+	glBindTexture(GL_TEXTURE_2D, m_shadowMap);
+}
+void	ShadowMapShader::Init()
+{
+	m_wvpLocation		=	GetUniformLocation("wvp");
+	m_textureLocation	=	GetUniformLocation("shadow");
+}
+void	ShadowMapShader::SetWVP(glm::mat4 const & wvp)
+{
+	glUniformMatrix4fv(m_wvpLocation, 1, GL_TRUE,glm::value_ptr(wvp));
+}
+void	ShadowMapShader::SetTextureUnit(unsigned int TextureUnit)
+{
+	glUniform1i(m_textureLocation, TextureUnit);
+}
