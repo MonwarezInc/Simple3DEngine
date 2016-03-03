e339842e61b6eb9ffad7f863b2c72b21ff933f29
Monwarez <monwarez@gmail.com>
2016-03-03 23:32:29 +0100

    Add main page documentation

---
+++ doc-gen/main.h
@@ -0,0 +1,13 @@
+/**
+*	\mainpage	Simple 3D Engine Source Code Documentation
+*	
+*# Simple3DEngine
+*
+*Simple 3D Engine is a bsd3-clauses library that aims to provide simple render system.<br/>
+*It use OpenGL version 3.2 + and GLSL 1.50 + in core profile.
+*
+*#	Supported Platform	
+*	* FreeBSD 
+*	* Linux
+*
+*/
