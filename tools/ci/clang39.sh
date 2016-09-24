09476a99c4f4670d4ebfaf375055431975311d6b
Monwarez <monwarez@gmail.com>
2016-09-24 19:37:11 +0200

    Update

--- tools/ci/clang39.sh
+++ tools/ci/clang39.sh
@@ -5,14 +5,17 @@ export CXX=clang++-3.9
 
 apt-get update -qq
 apt-get install -qq -y wget
+apt-get install -qq -y gnupg
+apt-get install -qq -y gnupg2
+apt-get install -qq -y gnupg1
 
  wget http://blog.anantshri.info/content/uploads/2010/09/add-apt-repository.sh.txt
  mv add-apt-repository.sh.txt add-apt-repository
  chmod +x add-apt-repository
- ./add-apt-repository -y 'ppa:zoogie/sdl2-snapshots'
- ./add-apt-repository ppa:ubuntu-toolchain-r/test -y
- ./add-apt-repository ppa:boost-latest/ppa -y
- ./add-apt-repository -y 'ppa:dartsim/ppa'
+ ./add-apt-repository  'ppa:zoogie/sdl2-snapshots'
+ ./add-apt-repository ppa:ubuntu-toolchain-r/test 
+ ./add-apt-repository ppa:boost-latest/ppa 
+ ./add-apt-repository 'ppa:dartsim/ppa'
  apt-get update -qq
  apt-get install -qq -y cmake
  apt-get install -qq -y make
@@ -22,4 +25,4 @@ apt-get install -qq -y wget
  apt-get install -qq -y libsdl2-image-dev
  apt-get install -qq -y libassimp-dev
 
- mkdir build && cd build && cmake . -DUSE_GLM_BUNDLE=TRUE && make
+ mkdir build && cd build && cmake .. -DUSE_GLM_BUNDLE=TRUE && make
