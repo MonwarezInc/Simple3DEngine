faf6c978fd592927e8ac1cbf545cbcfbd056ac03
Thibault Payet <monwarez@gmail.com>
2017-10-21 17:38:47 +0200

    add simple test for clang, add licence in new file

--- tests/Parser/test_parser.cpp
+++ tests/Parser/test_parser.cpp
@@ -1,3 +1,29 @@
+/*
+Copyright (c) 2017, Payet Thibault
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the Monwarez Inc nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL PAYET THIBAULT BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 #include "test_parser.h"
 
 void ParserTest::SetUp()
@@ -39,8 +65,19 @@ ParserTestParam createParameter( std::string toParseCouple, std::string toParseT
     ParserTestParam testParameter{toParseCouple, toParseTuple, couple, tuple};
     return testParameter;
 }
-INSTANTIATE_TEST_CASE_P( FloatingValue, ParserTest,
-                         testing::ValuesIn( {createParameter( "0,0", "1,2,3", {{0, 0}},
-                                                              {{1, 2, 3}} ),
-                                             createParameter( "0    ,    0", "   1,   100, 33.3 ",
-                                                              {{0, 0}}, {{1, 100, 33.3}} )} ) );
+INSTANTIATE_TEST_CASE_P(
+    FloatingValue, ParserTest,
+    testing::ValuesIn(
+        {createParameter( "0,0", "1,2,3", {{0, 0}}, {{1, 2, 3}} ),
+         createParameter( "0    ,    0", "   1,   100, 33.3 ", {{0, 0}}, {{1, 100, 33.3}} ),
+
+         createParameter( "0 ,  000000", "   1.00000,   100, 33.3 ", {{0, 0}}, {{1, 100, 33.3}} ),
+
+
+         createParameter( "00000,0", "   1000000.0001,   100, 33.3 ", {{0, 0}},
+                          {{1000000.0001, 100, 33.3}} ),
+
+
+         createParameter( "100,    0", "   1,   100, 33.3 ", {{100, 0}}, {{1, 100, 33.3}} ),
+         createParameter( "100,    50", "   1,   100, 33.3 ", {{100, 50}}, {{1, 100, 33.3}} ),
+         createParameter( "0    ,    40", "   1,   100, 33.3 ", {{0, 40}}, {{1, 100, 33.3}} )} ) );
