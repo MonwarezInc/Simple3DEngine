ade62e59eae70e4dbbdecbb61a2391ffdba13fca
Thibault Payet <monwarez@gmail.com>
2017-12-16 23:28:21 +0100

    use file in meson

--- tools/cppcheck.sh
+++ tools/cppcheck.sh
@@ -2,7 +2,7 @@
 cd $MESON_SOURCE_ROOT/src
 cppcheck --enable=all \
     --std=c++14 --language=c++ \
-    --xml $@ 2> $MESON_BUILD_ROOT/cppcheck.xml
+    --xml --project=$MESON_BUILD_ROOT/compile_commands.json 2> $MESON_BUILD_ROOT/cppcheck.xml
 
 cppcheck-htmlreport --file=$MESON_BUILD_ROOT/cppcheck.xml \
     --report-dir=$MESON_BUILD_ROOT/cppcheckReport
